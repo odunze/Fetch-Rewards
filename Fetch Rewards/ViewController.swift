1c15e7e21e2f0645c12d2bef933f1bd55dd69265
Lotanna Igwe Odunze <47992836+odunze@users.noreply.github.com>
2021-01-29 21:10:02 -0500

    Initial Commit

---
+++ Fetch Rewards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fetch Rewards
+//
+//  Created by Lotanna Igwe-Odunze on 1/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
